5e6d6cb015347553a080011486451f6d0957c87a
ubednama <shahidkhan.ak18@gmail.com>
2024-04-21 20:58:20 +0530

    updated circle-game

--- circle-game/index.js
+++ circle-game/index.js
@@ -18,12 +18,16 @@ function checkIntersection() {
 document.addEventListener('click', (e)=> {
     
     const allCircles = document.querySelectorAll(".circle");
+    const messageDiv = document.querySelector(".message")
     if(allCircles.length === 2){
         allCircles.forEach(circles => {
             document.body.removeChild(circles)
             circleCoor.shift();
             console.log(circleCoor)
         })
+        if (messageDiv) {
+            document.body.removeChild(messageDiv)
+        }
     }
 
     const x = e.clientX
@@ -43,8 +47,12 @@ document.addEventListener('click', (e)=> {
 
     document.body.appendChild(circle)
 
+    const message = document.createElement('div');
+    message.classList.add('message')
     if(circleCoor.length == 2){
         const res = checkIntersection()
+        message.innerHTML = `<b>Intersection is ${res}</b>`
+        document.body.appendChild(message)
         console.log("Intersection is ",res)
     }
 
