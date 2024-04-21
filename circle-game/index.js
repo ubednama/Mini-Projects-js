2bd25336194842daea973969d22df17098b9f922
ubednama <shahidkhan.ak18@gmail.com>
2024-04-21 20:36:20 +0530

    first commit

---
+++ circle-game/index.js
@@ -0,0 +1,51 @@
+const circleCoor = [];
+
+function checkIntersection() {
+    const circle1 = circleCoor[0];
+    const circle2 = circleCoor[1];
+
+    const x1 = circle1.x
+    const y1 = circle1.y
+    const r1 = circle1.r
+    
+    const x2 = circle2.x
+    const y2 = circle2.y
+    const r2 = circle2.r
+
+    return Math.hypot(x1-x2, y1-y2) <=r1+r2
+}
+
+document.addEventListener('click', (e)=> {
+    
+    const allCircles = document.querySelectorAll(".circle");
+    if(allCircles.length === 2){
+        allCircles.forEach(circles => {
+            document.body.removeChild(circles)
+            circleCoor.shift();
+            console.log(circleCoor)
+        })
+    }
+
+    const x = e.clientX
+    const y = e.clientY
+
+    // console.log(circleCoor)
+    
+    const randomRadius = Math.random() * 100;
+    circleCoor.push({x,y, r:randomRadius})
+    
+    const circle = document.createElement('div');
+    circle.classList.add('circle')
+    circle.style.top = y-randomRadius + "px"
+    circle.style.left = x-randomRadius + 'px'
+    circle.style.width = randomRadius*2 + 'px'
+    circle.style.height = randomRadius*2 + 'px'
+
+    document.body.appendChild(circle)
+
+    if(circleCoor.length == 2){
+        const res = checkIntersection()
+        console.log("Intersection is ",res)
+    }
+
+})
---
+++ tictactoe-js/README.md
@@ -0,0 +1,36 @@
+# tictactoe-js
+A simple web-based Tic Tac Toe game implemented using HTML, CSS, and JavaScript.
+
+#### Contact me
+email: [kubedrn@gmail.com](mailto:kubedrn@gmail.com)<br>
+<a href="https://www.linkedin.com/in/ubed9">
+  <!-- <img align="left" alt="Ubed's LinkedIN" width="22px" src="https://upload.wikimedia.org/wikipedia/commons/8/81/LinkedIn_icon.svg" /><br> -->
+
+## Demo
+Play game [here](https://65ae51a07077ff276e4d9826--deluxe-licorice-d069b8.netlify.app/)
+
+## Screenshots
+
+<p align="right" width="100%">
+  <img src="https://github.com/ubednama/tictactoe-js/assets/61332446/08a06a65-3ba6-4b30-b58c-9e6109bab482" alt="gameplay" width="49%" align="center"  />
+  <img src="https://github.com/ubednama/tictactoe-js/assets/61332446/38453222-d13e-4a60-8b4c-f2778eaa7289" alt="winner dashboard" width="49%" align="center"  />
+</p>
+
+
+## Features
+
+- Two-player mode for local multiplayer
+- Responsive design for various screen sizes
+
+## To DO
+- ~~Add Random Quotes at bottom~~ (✅Done)
+- Add Computer vs Human mode
+
+## Contributing
+If you'd like to contribute to this project, please follow these steps:
+
+- Fork the repository.
+- Create a new branch for your feature.
+- Make your changes and commit them.
+- Push to the branch.
+- Submit a pull request.
